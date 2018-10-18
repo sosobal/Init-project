ce560abb7b1db44765ce3d2b836ca3d54d97cc5c
sosobal <sosobal1024@naver.com>
2018-10-18 12:52:48 +0900

    [2018-10-18] complete TTS

--- app/src/main/java/com/example/a507/myapplication1002/MainActivity.java
+++ app/src/main/java/com/example/a507/myapplication1002/MainActivity.java
@@ -5,19 +5,24 @@ import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
-protected Button btHomepage, btDial, btCall, btSms, btMap, btRecog;
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+protected Button btHomepage, btDial, btCall, btSms, btMap, btRecog, btTts;
 protected TextView tvRecog;
+protected EditText etTts;
+protected TextToSpeech tts;
 private static int CODE_RECOG = 1215;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,11 +85,22 @@ private static int CODE_RECOG = 1215;
                 voiceRecog()
             }
         });
+        etTts = (EditText) findViewById(R.id.edTts);
+        btTts = (Button) findViewById(R.id.btTts);
+        btTts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = etTts.getText().toString();
+                tts.speak(str, TextToSpeech.QUEUE_FLUSH,null,null);
+            }
+        });
+        tts = new TextToSpeech(this,this);
             }
             private void voiceRecog() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREA);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please speak.");
         startActivityForResult(intent,CODE_RECOG);
             }
 
@@ -101,6 +117,15 @@ private static int CODE_RECOG = 1215;
 
         }
 
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.KOREAN);
+            tts.setPitch(1.0f);
+            tts.setSpeechRate(1.0f);
+        }
     }
 }
 
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-10-18] add the Recog"
+git commit -m "[2018-10-18] complete TTS"
 git push -u origin master
