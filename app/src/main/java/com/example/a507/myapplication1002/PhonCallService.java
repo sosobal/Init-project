22b23a6e58e9d1a75751185093e8d8567db391af
sosobal <sosobal1024@naver.com>
2018-11-20 16:36:18 +0900

    [2018-11-20]  start checking outgoing calls

--- app/src/main/java/com/example/a507/myapplication1002/PhonCallService.java
+++ app/src/main/java/com/example/a507/myapplication1002/PhonCallService.java
@@ -2,19 +2,27 @@ package com.example.a507.myapplication1002;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 
 public class PhonCallService extends Service {
+    protected PhoneCallReceiver phoneCallReceiver;
+
     public PhonCallService() {
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        int nResult = super.onStartCommand(intent, flags, startId);
+        phoneCallReceiver = new PhoneCallReceiver();
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
+        registerReceiver(phoneCallReceiver, intentFilter);
+        return nResult;
     }
 
     @Override
     public void onDestroy() {
+        unregisterReceiver(phoneCallReceiver);
         super.onDestroy();
     }
 
