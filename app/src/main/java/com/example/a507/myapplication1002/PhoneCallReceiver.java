41800a283c55e22d800de71482907af64bb21fa2
sosobal <sosobal1024@naver.com>
2018-11-20 16:42:45 +0900

    [2018-11-20] finish checking outgoing calls

--- app/src/main/java/com/example/a507/myapplication1002/PhoneCallReceiver.java
+++ app/src/main/java/com/example/a507/myapplication1002/PhoneCallReceiver.java
@@ -3,6 +3,7 @@ package com.example.a507.myapplication1002;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class PhoneCallReceiver extends BroadcastReceiver {
 
@@ -11,5 +12,13 @@ public class PhoneCallReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         //throw new UnsupportedOperationException("Not yet implemented");
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+            String sPhoneNum = intent.getStringExtra(intent.EXTRA_PHONE_NUMBER);
+            Toast.makeText(context, "Phone # = " + sPhoneNum, Toast.LENGTH_SHORT).show();
+        } else if (action.equals(Intent.ACTION_CALL_BUTTON)) {
+            String sKey = intent.getStringExtra(intent.EXTRA_PHONE_NUMBER);
+            Toast.makeText(context, "Key = " + sKey, Toast.LENGTH_SHORT).show();
+        }
     }
 }
